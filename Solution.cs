a11be7f0a223928630ab8dff46b46f3dc48505be
konrad300 <konrad300@users.noreply.github.com>
2017-05-04 02:25:02 +0200

    Initial

---
+++ Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace CodilityStarter
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            return 0;
+        }
+
+        public static IEnumerable<object[]> Test1Data => new[]
+        {
+            new object[] {new[] {0, 0, 0}},
+            new object[] {new[] {0, 0, 0}},
+        };
+
+        [Theory]
+        [MemberData(nameof(Test1Data))]
+        public void Test1_Description(int[] input)
+        {
+            var solution = new Solution();
+            var result = solution.solution(input);
+
+            var expected = 0;
+            result.ShouldBeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Test2_Description()
+        {
+            var solution = new Solution();
+            var input = new int[] {};
+            var result = solution.solution(input);
+
+            var expected = 0;
+            result.ShouldBeEquivalentTo(expected);
+        }
+    }
+}
